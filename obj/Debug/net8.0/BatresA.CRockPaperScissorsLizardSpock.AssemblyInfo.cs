79c81310f6cdc46dfdacccfed6bbc7fed2a8eed5
AlanBatres1 <alanbatres328@gmail.com>
2025-01-15 11:56:33 -0800

    gitignore

--- obj/Debug/net8.0/BatresA.CRockPaperScissorsLizardSpock.AssemblyInfo.cs
+++ obj/Debug/net8.0/BatresA.CRockPaperScissorsLizardSpock.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatresA.CRockPaperScissorsLizardSpock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d86dd0a4749ebeeb842ae5bb144288c22a776e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatresA.CRockPaperScissorsLizardSpock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatresA.CRockPaperScissorsLizardSpock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
